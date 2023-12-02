d664d7da50c60268f11fbfcc3ffe4e8547d72904
ImperatorMarsa <tima.sitnikov@mail.ru>
2023-12-02 10:54:23 +0300

    добавил выделение цифер из строчек

--- 2023/Day1/puzzle_1/src/main.rs
+++ 2023/Day1/puzzle_1/src/main.rs
@@ -1,29 +1,44 @@
 use core::panic;
 use std::{
     fs::File,
-    io::{BufReader, Read},
+    io::{self, BufRead},
+    path::Path,
 };
 
 fn main() {
-    let content = match File::open("input/input.txt") {
-        Ok(file) => file,
-        Err(error) => panic!("Problem opening the file: {:?}", error),
-    };
+    if let Ok(lines) = read_lines("input/input.txt") {
+        for line in lines {
+            if let Ok(raw_calibration_value) = line {
+                let mut raw_calibration_value_chars = raw_calibration_value.chars();
 
-    let mut reader = BufReader::new(content);
-    let mut raw_calibration_value = String::new();
-    let mut num_bytes = match reader.read_to_string(&mut raw_calibration_value) {
-        Ok(readed_line) => readed_line,
-        Err(error) => panic!("Reading from cursor won't fail: {:?}", error),
-    };
-
-    while num_bytes > 0 {
-        println!("{}", raw_calibration_value);
-
-        raw_calibration_value.clear();
-        num_bytes = match reader.read_to_string(&mut raw_calibration_value) {
-            Ok(readed_line) => readed_line,
-            Err(error) => panic!("Reading from cursor won't fail: {:?}", error),
-        };
+                let mut calibration_value = String::new();
+                loop {
+                    match raw_calibration_value_chars.next() {
+                        Some(result) => {
+                            let tmp_string = String::from(result);
+                            match tmp_string.parse::<u32>() {
+                                Ok(_) => calibration_value.push(result),
+                                Err(_) => (),
+                            };
+                        }
+                        None => {
+                            break;
+                        }
+                    };
+                }
+                println!(
+                    "calibration_value {} {}",
+                    calibration_value, raw_calibration_value
+                );
+            }
+        }
     }
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
 }
