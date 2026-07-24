051bb1d26b0d578d0b48d5d9b48224a4bc59a2d2
Sitnikov Timofey <t.sitnikov@infocom-s.ru>
2026-07-24 10:05:49 +0300

    Добавил функцию определения повторяющегося паттерна

--- 2025/php/src/Day02/Solver.php
+++ 2025/php/src/Day02/Solver.php
@@ -10,6 +10,9 @@ use Override;
 
 final class Solver implements Solution
 {
+    private const RANGE_SEPARATOR = ',';
+    private const ID_SEPARATOR = '-';
+
     #[Override]
     public function solvePartOne(string $input): int
     {
@@ -20,5 +23,22 @@ final class Solver implements Solution
     public function solvePartTwo(string $input): int
     {
         throw new Exception('Not implemented');
+    }
+
+    public function isRepeatedPattern(int $id): ?int
+    {
+        $sId = ltrim(trim(strval($id)), '0');
+        $idLen = strlen($sId);
+        if ($idLen % 2) {
+            return null;
+        }
+
+        $leftHalf = substr($sId, 0, $idLen / 2);
+        $rightHalf = substr($sId, $idLen / 2);
+        if ($leftHalf == $rightHalf) {
+            return $id;
+        }
+
+        return null;
     }
 }
